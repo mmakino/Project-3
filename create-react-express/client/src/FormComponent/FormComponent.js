7de5e5135b42c0c7d74262341cc8dc881de32af2
Jr <jgrantlyons@gmail.com>
2019-03-22 20:22:17 -0700

    material-ui

---
+++ create-react-express/client/src/FormComponent/FormComponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import FilledInput from '@material-ui/core/FilledInput';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+
+
+const TitleComponent = (props) => {
+    return (
+
+        <div className='TitleComponent'>
+
+        </div>
+
+    )
+};
+
+export default TitleComponent;
