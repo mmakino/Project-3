9bde4576967fcd339e314b097af5a313f95e6216
SNAFU <nholdsworth@me.com>
2019-03-22 20:29:49 -0700

    pulling latest master

--- create-react-express/client/models/Alcohol.js
+++ create-react-express/client/models/Alcohol.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-    var AlcoholTable = sequelize.define("Alcohol Table", {
+    var AlcoholTable = sequelize.define("Alcohol", {
         type: DataTypes.STRING,
         brandStyle: DataTypes.STRING,
         emptyBottleWeight: DataTypes.INTEGER,
