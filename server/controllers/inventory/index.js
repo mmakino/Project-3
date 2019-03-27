542a25ce49f8320c5bdf4bb047488d7e865565eb
Motohiko Makino <motohiko.makino@gmail.com>
2019-03-26 17:32:53 -0700

    Added some more simple routes for POST, GET, etc.
    * /api/inventory POST allows direction insertion
      from client by passing JSON
      {
        "userId": "1",
        "type": "Burbon",
        "brandStyle": "Jack Daniel",
        "sizeML": "750",          <== REQUIRED
        "sizeOZ": "25.36",        <== REQUIRED
        "totalFullBottles": "3",
        "costPerBottle": "33.33", <== REQUIRED
        "updatedAt": "2019-03-27T00:12:47.759Z",
        "createdAt": "2019-03-27T00:12:47.759Z"
      }
    * Other available simple/direct routes are
      * GET    /api/inventory/:inventoryId
      * UPDATE /api/inventory/:inventoryId
      * DELETE /api/inventory/:inventoryId

--- server/controllers/inventory/index.js
+++ server/controllers/inventory/index.js
@@ -1,15 +1,74 @@
 //
 // Controller for inventory data API
 //
+// !!! NOT SECURE as of 2019-03-26 !!!
+// --> CURRENTLY ALL CONTROLLERS ARE NOT USER SPECIFIC
+//
 
 // Load database models
 const db = require("../../db/models");
 
-module.exports = {
-  // Retrieve all rows of UserInventory 
-  findAll: function(req, res) {
+//
+// UserInventory Controller
+//
+class UserInventoryController {
+  //
+  // Return all rows of UserInventory
+  //
+  findAll(req, res) {
     db.UserInventory.findAll(req.query)
       .then(data => res.json(data))
       .catch(err => res.status(422).json(err));
-  },
+  }
+
+  //
+  // Find by UserInventory ID
+  //
+  findByInvId(req, res) {
+    db.UserInventory.findByPk(req.params.id)
+      .then(data => res.json(data))
+      .catch(err => res.status(422).json(err));
+  }
+
+  //
+  // Insert a new model data 
+  //
+  create(req, res) {
+    db.UserInventory.create(req.body)
+      .then(data => res.json(data))
+      .catch(err => res.status(422).json(err));
+  }
+
+  //
+  // Update the specified UserInventory ID
+  //
+  updateByInvID(req, res) {
+    const options = {
+      where: {
+        id: req.params.id
+      }
+    };
+  
+    db.UserInventory.update(req.body, options)
+      .then(data => res.json(data))
+      .catch(err => res.status(422).json(err));
+  }
+
+  //
+  // Delete the specified UserInventory ID
+  //
+  deleteByInvID(req, res) {
+    const options = {
+      where: {
+        id: req.params.id
+      }
+    };
+
+    db.UserInventory.destroy(options)
+      .then(data => res.json(data))
+      .catch(err => res.status(422).json(err));
+  }
 }
+
+module.exports = new UserInventoryController();
+
