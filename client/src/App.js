495ff6361d14d5583c9f3eb23747966d4b48cc0d
Nixinerix <xoamara2@gmail.com>
2019-03-27 20:18:19 -0700

    React allowing inputs but not updating state in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,67 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import './App.css';
-import LiquidAssets from './LiquidAssets'
-import NavbarComponent from './NavbarComponent'
+import LiquidAssets from './LiquidAssets';
+import NavbarComponent from './NavbarComponent';
+import axios from 'axios';
+import FormComponent from "./LiquidAssets/FormComponent";
+import ImageComponent from "./LiquidAssets/ImageComponent";
+import TableComponent from "./LiquidAssets/TableComponent";
+
+
 
 class App extends Component {
+        state = {
+            brandStyle: ``,
+            bottleSize: ``,
+            unopenedBottles: ``,
+            bottleCost: ``,
+            bottleWeight: ``,
+            percentageLeft: ``,
+            totalBrandStyleValue: ``
+        };
+
+
+    // TODO: this needs to go inside of a onClick handler function that can be passed into the button.  This will post the state of the form to the route that I choose the post route to be.  Might have to make a variable and put the states into a variable
+
+    componentDidMount() {
+      this.getAlcohol();
+    }
+
+    getAlcohol = () => {
+      return axios.get('/api/alcohol')
+          .then((response) => {
+              console.log(response);
+              this.setState({
+                brandStyle: response.brandStyle,
+                bottleSize: response.bottleSize,
+              })
+          })
+          .catch((error) => {
+              console.log(error);
+          });
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+    postToInventory = () => {
+        return axios.post('/api/inventory', {
+            brandStyle: this.state.brandStyle
+        })
+            .then((response) => {
+                console.log(response)
+            })
+    }
+
+    check = () => {
+        console.log(this.state)
+    }
   
   render() {
-
-    const { classes } = this.props;
 
     return (
       <div className="App">
@@ -15,6 +69,12 @@ class App extends Component {
         <NavbarComponent />
 
         <LiquidAssets />
+        <FormComponent 
+        handleInputChange={this.handleInputChange}
+
+        />
+        <ImageComponent />
+        <TableComponent />
 
       </div>
     );
