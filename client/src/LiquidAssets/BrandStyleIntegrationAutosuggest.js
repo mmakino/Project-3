27ed90b1861ec2b55e7e4a8ede3d9d652615f814
Motohiko Makino <motohiko.makino@gmail.com>
2019-03-31 20:49:57 -0700

    stuck at form

--- client/src/LiquidAssets/BrandStyleIntegrationAutosuggest.js
+++ client/src/LiquidAssets/BrandStyleIntegrationAutosuggest.js
@@ -1,313 +1,85 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import deburr from 'lodash/deburr';
 import Autosuggest from 'react-autosuggest';
-import match from 'autosuggest-highlight/match';
-import parse from 'autosuggest-highlight/parse';
-import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
-import MenuItem from '@material-ui/core/MenuItem';
-import Popper from '@material-ui/core/Popper';
-import { withStyles } from '@material-ui/core/styles';
-import axios from 'axios'
+import {withStyles} from '@material-ui/core/styles';
 
-// ****************************************************************************************THIS IS ONLY NECESSARY BEFORE WE CUSTOMIZED THIS FOR OUR AXIOS REQUEST TO DYNAMICALLY RENDER IT, HOWEVER THE FORMAT BELOW IS HOW YOUR DATA MUST LOOK  ****************************************************************************************
-// let suggestions = [
-//     { label: 'Afghanistan' },
-//     { label: 'Aland Islands' },
-//     { label: 'Albania' },
-//     { label: 'Algeria' },
-//     { label: 'American Samoa' },
-//     { label: 'Andorra' },
-//     { label: 'Angola' },
-//     { label: 'Anguilla' },
-//     { label: 'Antarctica' },
-//     { label: 'Antigua and Barbuda' },
-//     { label: 'Argentina' },
-//     { label: 'Armenia' },
-//     { label: 'Aruba' },
-//     { label: 'Australia' },
-//     { label: 'Austria' },
-//     { label: 'Azerbaijan' },
-//     { label: 'Bahamas' },
-//     { label: 'Bahrain' },
-//     { label: 'Bangladesh' },
-//     { label: 'Barbados' },
-//     { label: 'Belarus' },
-//     { label: 'Belgium' },
-//     { label: 'Belize' },
-//     { label: 'Benin' },
-//     { label: 'Bermuda' },
-//     { label: 'Bhutan' },
-//     { label: 'Bolivia, Plurinational State of' },
-//     { label: 'Bonaire, Sint Eustatius and Saba' },
-//     { label: 'Bosnia and Herzegovina' },
-//     { label: 'Botswana' },
-//     { label: 'Bouvet Island' },
-//     { label: 'Brazil' },
-//     { label: 'British Indian Ocean Territory' },
-//     { label: 'Brunei Darussalam' },
-// ];
 
-function renderInputComponent(inputProps) {
-    const { classes, inputRef = () => { }, ref, ...other } = inputProps;
-
-    return (
-        <TextField
-            id='standard-name'
-            label='Please enter the Brand Of Booze'
-            variant='outlined'
-            className={classes.textField}
-            fullWidth
-            InputProps={{
-                inputRef: node => {
-                    ref(node);
-                    inputRef(node);
-                },
-                classes: {
-                    input: classes.input,
-                },
-            }}
-            {...other}
-        />
-    );
-}
-
-function renderSuggestion(suggestion, { query, isHighlighted }) {
-    const matches = match(suggestion.label, query);
-    const parts = parse(suggestion.label, matches);
-
-    return (
-        <MenuItem selected={isHighlighted} component="div">
-            <div>
-                {parts.map((part, index) =>
-                    part.highlight ? (
-                        <span key={String(index)} style={{ fontWeight: 500 }}>
-                            {part.text}
-                        </span>
-                    ) : (
-                            <strong key={String(index)} style={{ fontWeight: 300 }}>
-                                {part.text}
-                            </strong>
-                        ),
-                )}
-            </div>
-        </MenuItem>
-    );
-}
-
-// THIS IS WHERE THIS FUNCTION WHICH IS NOW INSIDE THE CLASS BELOW ORIGINALLY WAS!*****************************************************************************
-// function getSuggestions(value) {
-//     const inputValue = deburr(value.trim()).toLowerCase();
-//     const inputLength = inputValue.length;
-//     let count = 0;
-
-//     return inputLength === 0
-//         ? []
-//         : suggestions.filter(suggestion => {
-//             const keep =
-//                 count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
-
-//             if (keep) {
-//                 count += 1;
-//             }
-
-//             return keep;
-//         });
-// }
-
-function getSuggestionValue(suggestion) {
-    return suggestion.label;
-}
-
+//
+// Auto-suggest text box styles
+//
 const styles = theme => ({
-    root: {
-        height: 250,
-        flexGrow: 1,
-    },
-    container: {
-        position: 'relative',
-    },
-    suggestionsContainerOpen: {
-        position: 'absolute',
-        zIndex: 1,
-        marginTop: theme.spacing.unit,
-        left: 0,
-        right: 0,
-    },
-    suggestion: {
-        display: 'block',
-    },
-    suggestionsList: {
-        margin: 0,
-        padding: 0,
-        listStyleType: 'none',
-    },
-    divider: {
-        height: theme.spacing.unit * 2,
-    },
+  root: {
+    height: 250,
+    flexGrow: 1,
+  },
+  container: {
+    padding: theme.spacing.unit * 1.5,
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  suggestion: {
+    display: 'block',
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none',
+  },
+  divider: {
+    height: theme.spacing.unit * 2,
+  },
 });
 
 class BrandStyleIntegrationAutosuggest extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            single: '',
-            popper: '',
-            // we added this part, this suggestions array is where our data from the axios call gets pushed.  There was already a suggestions array but we modified our in order to conform to the code we had already written.
-            autosuggest: {
-                suggestions: []
-            }
-
-        };
-    }
-
-    // componentDidMount is where we 
-    componentDidMount() {
-        this.getBoozeSuggestions().then( (gottenBoozeSuggestions) => {
-            this.setState({
-                autosuggest: {
-                    suggestions: gottenBoozeSuggestions
-                }
-            });
-        })
-    }
-
-
-    getBoozeSuggestions = () => {
-        // This constructor function returns a promise containing a function that returns our data once the axios call has been resolved so that data can be passed into other areas such as in line 169 above.  "resolved" and then maps over the response.data.     
-        return new Promise((resolve, reject) => {
-            axios.get('/api/alcohol')
-                .then((response) => {
-                    // We set gottenBoozeSuggestions equal to a  map that maps over the data from the api call which we will return from the function.  Map takes one argument that can be named whatever you want.  We are next setting the value of brandstyle from the response to the 'label:' key because this is the format that we need our data in for the IntegrationAutosuggest component to handle it correctly.
-                    let gottenBoozeSuggestions = response.data.map( (boozeData) => {
-                        // We are returning the data in exactly the format that we need it in for the suggestions array in our state above, so that it plugs in nicely to the component.
-                        return { label: boozeData.brandstyle }
-                    })
-                    console.log(gottenBoozeSuggestions)
-                    // this resolve method is where we are passing 
-                    resolve(gottenBoozeSuggestions)
-
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        })
-
-    }
-
-    getSuggestions(value) {
-        const inputValue = deburr(value.trim()).toLowerCase();
-        const inputLength = inputValue.length;
-        let count = 0;
-
-        return inputLength === 0
-            ? []
-            // WE HAD TO MODIFY THIS BECUASE WE MOVED THIS PROPERTY INSIDE THIS CLASS.  IT WAS OUTSIDE THIS CLASS BEFORE
-            : this.state.autosuggest.suggestions.filter(suggestion => {
-                const keep =
-                    count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
-
-                if (keep) {
-                    count += 1;
-                }
-
-                return keep;
-            });
-    }
-
-    handleSuggestionsFetchRequested = ({ value }) => {
-        this.setState({
-            suggestions: this.getSuggestions(value),
-        });
+  constructor (props) {
+    super (props);
+    this.state = {
+      inputValue: '',
+      // we added this part, this suggestions array is where our data from the axios call gets pushed.  There was already a suggestions array but we modified our in order to conform to the code we had already written.
+    //   suggestions: [],
     };
+  }
 
-    handleSuggestionsClearRequested = () => {
-        this.setState({
-            suggestions: [],
-        });
-    };
 
-    handleChange = name => (event, { newValue }) => {
-        this.setState({
-            [name]: newValue,
-        });
-    };
+  render () {
+    const {classes, autosuggestProps} = this.props;
 
-    render() {
-        const { classes } = this.props;
-
-        const autosuggestProps = {
-            renderInputComponent,
-            // ALSO HERE WE HAD TO UPDATE FROM suggestions: 'this.suggestion' to what we wrote below ...
-            suggestions: this.state.autosuggest.suggestions,
-            onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
-            onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
-            getSuggestionValue,
-            renderSuggestion,
-        };
-
-        return (
-            <div className={classes.root}>
-                <Autosuggest
-                    {...autosuggestProps}
-                    inputProps={{
-                        classes,
-                        placeholder: 'Search a country (start with a)',
-                        value: this.state.single,
-                        onChange: this.handleChange('single'),
-                    }}
-                    theme={{
-                        container: classes.container,
-                        suggestionsContainerOpen: classes.suggestionsContainerOpen,
-                        suggestionsList: classes.suggestionsList,
-                        suggestion: classes.suggestion,
-                    }}
-                    renderSuggestionsContainer={options => (
-                        <Paper {...options.containerProps} square>
-                            {options.children}
-                        </Paper>
-                    )}
-                />
-                <div className={classes.divider} />
-                <Autosuggest
-                    {...autosuggestProps}
-                    inputProps={{
-                        classes,
-                        label: 'Label',
-                        placeholder: 'With Popper',
-                        value: this.state.popper,
-                        onChange: this.handleChange('popper'),
-                        inputRef: node => {
-                            this.popperNode = node;
-                        },
-                        InputLabelProps: {
-                            shrink: true,
-                        },
-                    }}
-                    theme={{
-                        suggestionsList: classes.suggestionsList,
-                        suggestion: classes.suggestion,
-                    }}
-                    renderSuggestionsContainer={options => (
-                        <Popper anchorEl={this.popperNode} open={Boolean(options.children)}>
-                            <Paper
-                                square
-                                {...options.containerProps}
-                                style={{ width: this.popperNode ? this.popperNode.clientWidth : null }}
-                            >
-                                {options.children}
-                            </Paper>
-                        </Popper>
-                    )}
-                />
-            </div>
-        );
-    }
+    return (
+      <Autosuggest
+        {...autosuggestProps}
+        inputProps={{
+          classes,
+          label: this.props.label,
+          placeholder: this.props.placeholder,
+        //   value: this.state.inputValue,
+        //   onChange: this.props.handleChange ('inputValue'),
+          value: this.props.value,
+          onChange: this.props.onChange,
+        }}
+        theme={{
+          container: classes.container,
+          suggestionsContainerOpen: classes.suggestionsContainerOpen,
+          suggestionsList: classes.suggestionsList,
+          suggestion: classes.suggestion,
+        }}
+        renderSuggestionsContainer={options => (
+          <Paper {...options.containerProps} square>
+            {options.children}
+          </Paper>
+        )}
+      />
+    );
+  }
 }
 
 BrandStyleIntegrationAutosuggest.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(BrandStyleIntegrationAutosuggest);
+export default withStyles (styles) (BrandStyleIntegrationAutosuggest);
--- client/src/LiquidAssets/FormComponent.js
+++ client/src/LiquidAssets/FormComponent.js
@@ -1,151 +1,229 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, TextField, Grid, Paper, Button } from '@material-ui/core';
-import BrandStyleIntegrationAutosuggest from './BrandStyleIntegrationAutosuggest'
+import {withStyles, TextField, Grid, Paper, Button} from '@material-ui/core';
+import deburr from 'lodash/deburr';
+import BrandStyleIntegrationAutosuggest
+  from './BrandStyleIntegrationAutosuggest';
+
+import getBoozeSuggestions from './autosuggest/queryBooze';
+import {
+  renderInputComponent,
+  renderSuggestion,
+  getSuggestionValue,
+} from './autosuggest/renderProps';
 
 const styles = theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-    },
-    dense: {
-        marginTop: 16,
-    },
-    menu: {
-        width: 200,
-    }
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
 });
 
 const nateStyles = {
-    paper: {
-        padding: 40,
-        marginTop: 30,
-        marginRight: 62
-    },
-    gridContainer: {
-        marginTop: 20,
-        marginLeft: 40,
-        padding: 20,
-    }
-
-}
-
-
-
-const FormComponent = (props) => {
-
-    return (
-        <Grid container style={nateStyles.gridContainer}>
-            <Grid item xs>
-                <Paper style={nateStyles.paper}>
-
-                    <form className={'form-container'} noValidate
-                    // Not sure how this works.
-                    autoComplete="off">
-                        {/* THIS IS HOW WE ACCOMPLISHED AUTOCOMPLETE */}
-                        <BrandStyleIntegrationAutosuggest
-                            value={props.formInputs.brandstyle}
-                            onChange={props.handleInputChange}
-                            name="brandStyle"
-                            getBoozeSuggestions={props.getBoozeSuggestions}
-                            autosuggest={props.autosuggest}
-                        />
-
-                        <TextField
-                            id="outlined-full-width"
-                            label="What size bottle is this?  750mL 1000mL or some weird ass size?"
-                            style={{ margin: 5 }}
-                            placeholder="750 ml"
-                            helperText="we'll let you know exactly how many ounces of booze you got in that there bottle."
-                            fullWidth
-                            margin="normal"
-                            variant="outlined"
-                            value={props.formInputs.bottleSize}
-                            name="bottleSize"
-                            onChange={props.handleInputChange}
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-
-                        <TextField
-                            id="outlined-full-width"
-                            label="How many extra bottles of this you got stashed away? Not including this opened bottle"
-                            style={{ margin: 5 }}
-                            placeholder="12"
-                            helperText="this helps us calculate the total dollar value in your possesion for this product"
-                            fullWidth
-                            margin="normal"
-                            variant="outlined"
-                            name="unopenedBottles"
-                            value={props.formInputs.unopenedBottles}
-                            onChange={props.handleInputChange}
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-
-                        <TextField
-                            id="outlined-full-width"
-                            label="How much does this sauce cost you per bottle?"
-                            style={{ margin: 5 }}
-                            placeholder="17.00"
-                            helperText="We will tell you your cost per ounce!"
-                            fullWidth
-                            margin="normal"
-                            variant="outlined"
-                            name="bottleCost"
-                            value={props.formInputs.bottleCost}
-                            onChange={props.handleInputChange}
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-
-
-
-                        <TextField
-                            id="outlined-full-width"
-                            label="Throw it up on the scale and type in what it weighs, in gramz pleeze."
-                            style={{ margin: 5 }}
-                            placeholder="1432g"
-                            helperText="So much better than eyeball estimation.  When you go to the bank, and ask for your balance do you want the banker to be like: 'You got somewhere between like 10 and 15 thousand dollars you know give or take'.  Yeah, didn't think so... that's hella inaccurate."
-                            fullWidth
-                            margin="normal"
-                            variant="outlined"
-                            name="bottleWeight"
-                            value={props.formInputs.bottleWeight}
-                            onChange={props.handleInputChange}
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                        />
-
-                        <Grid item
-                            align='right'
-                            style={{ marginTop: 20 }}>
-                            <Button
-                                variant="contained"
-                                className='button'
-                                onClick={props.getUserInventory}>
-
-                                Send To Inventory
-                                </Button>
-                        </Grid>
-
-                    </form>
-                </Paper>
-            </Grid>
-        </Grid>
-    )
-}
-    ;
-FormComponent.propTypes = {
-    classes: PropTypes.object.isRequired,
+  paper: {
+    padding: 40,
+    marginTop: 30,
+    marginRight: 62,
+  },
+  gridContainer: {
+    marginTop: 20,
+    marginLeft: 40,
+    padding: 20,
+  },
 };
 
-export default withStyles(styles)(FormComponent);
+// const FormComponent = props => {
+class FormComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        brandStyle: '',
+        bottleSize: -1,
+        // we added this part, this suggestions array is where our data from the axios call gets pushed.  There was already a suggestions array but we modified our in order to conform to the code we had already written.
+        suggestions: [],
+      };  
+  }
+
+  initSuggestions() {
+    getBoozeSuggestions()
+      .then(gottenBoozeSuggestions => {
+        this.setState({
+          suggestions: gottenBoozeSuggestions
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  // componentDidMount is where we
+  componentDidMount() {
+    this.initSuggestions();
+  }
+
+  getSuggestions(value) {
+    const inputValue = deburr(value.trim()).toLowerCase();
+    const inputLength = inputValue.length;
+    let count = 0;
+
+    return inputLength === 0
+      ? []
+      : // WE HAD TO MODIFY THIS BECUASE WE MOVED THIS PROPERTY INSIDE THIS CLASS.  IT WAS OUTSIDE THIS CLASS BEFORE
+        this.state.suggestions.filter(suggestion => {
+          const keep =
+            count < 5 &&
+            suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
+
+          if (keep) {
+            count += 1;
+          }
+
+          return keep;
+        });
+  }
+
+  handleSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  };
+
+  handleSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  handleChange = name => (event, { newValue }) => {
+    if (this.state.suggestions.length === 0) {
+      this.initSuggestions();
+    }
+    this.setState({
+      [name]: newValue
+    });
+  };
+
+  render() {
+    const autosuggestProps = {
+      renderInputComponent,
+      // ALSO HERE WE HAD TO UPDATE FROM suggestions: 'this.suggestion' to what we wrote below ...
+      suggestions: this.state.suggestions,
+      onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
+      onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
+      getSuggestionValue,
+      renderSuggestion
+    };
+
+    return (
+      <Grid container style={nateStyles.gridContainer}>
+        <Grid item xs>
+          <Paper style={nateStyles.paper}>
+            <form
+              className={"form-container"}
+              noValidate
+              // Not sure how this works.
+              autoComplete="off"
+            >
+              {/* THIS IS HOW WE ACCOMPLISHED AUTOCOMPLETE */}
+              <BrandStyleIntegrationAutosuggest
+                id="brandStyle"
+                label="Input the brand of booze you be weighin' and it's vintage or style here playa!"
+                placeholder="Pappy Van Winkle 15 yr"
+                value={this.state.brandStyle}
+                onChange={() => this.handleChange('brandStyle')}
+                name="brandStyle"
+                autosuggestProps={autosuggestProps}
+              />
+
+              <BrandStyleIntegrationAutosuggest
+                id="bottleSize"
+                label="What size bottle is this?  750mL 1000mL or some weird ass size?"
+                placeholder="750 ml"
+                helperText="we'll let you know exactly how many ounces of booze you got in that there bottle."
+                name="bottleSize"
+                value={this.state.bottleSize}
+                onChange={() => this.handleChange('bottleSize')}
+                autosuggestProps={autosuggestProps}
+              />
+
+              <TextField
+                id="outlined-full-width"
+                label="How many extra bottles of this you got stashed away? Not including this opened bottle"
+                style={{ margin: 5 }}
+                placeholder="12"
+                helperText="this helps us calculate the total dollar value in your possesion for this product"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                name="unopenedBottles"
+                value={this.props.formInputs.unopenedBottles}
+                onChange={this.props.handleInputChange}
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
+
+              <TextField
+                id="outlined-full-width"
+                label="How much does this sauce cost you per bottle?"
+                style={{ margin: 5 }}
+                placeholder="17.00"
+                helperText="We will tell you your cost per ounce!"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                name="bottleCost"
+                value={this.props.formInputs.bottleCost}
+                onChange={this.props.handleInputChange}
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
+
+              <TextField
+                id="outlined-full-width"
+                label="Throw it up on the scale and type in what it weighs, in gramz pleeze."
+                style={{ margin: 5 }}
+                placeholder="1432g"
+                helperText="So much better than eyeball estimation.  When you go to the bank, and ask for your balance do you want the banker to be like: 'You got somewhere between like 10 and 15 thousand dollars you know give or take'.  Yeah, didn't think so... that's hella inaccurate."
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                name="bottleWeight"
+                value={this.props.formInputs.bottleWeight}
+                onChange={this.props.handleInputChange}
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
+
+              <Grid item align="right" style={{ marginTop: 20 }}>
+                <Button
+                  variant="contained"
+                  className="button"
+                  onClick={this.props.getUserInventory}
+                >
+                  Send To Inventory
+                </Button>
+              </Grid>
+            </form>
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+FormComponent.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles (styles) (FormComponent);
