b812363bd8db42c28a6b71d672ad548c9860ecf7
Motohiko Makino <motohiko.makino@gmail.com>
2019-04-05 11:52:58 -0700

    Update for heroku deploy
    * Updated server/index.js for heroku deployment
    * Fixed bottle size suggestions
    * Improved brandstyle image loading

--- server/index.js
+++ server/index.js
@@ -41,10 +41,16 @@ require("./routes/api/test/auth")(app);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-// *** UNCOMMENT THIS ROUTE ONLY WHEN PRODUCTION DISPLOYMENT ***
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('test');
+  });
+}
 
 db.sequelize.sync().then(() => {
   httpServer.listen(PORT, console.log(`Server listening on PORT ${PORT}`));
