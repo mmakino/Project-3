42c407bc4ad3d2d72c9efbae994ce7402d5df3e2
JAngel <43564263+jenaym@users.noreply.github.com>
2019-04-04 20:25:24 -0700

    CSV personalized headers

--- server/db/migrations/20190323025740-create-user-inventories.js
+++ server/db/migrations/20190323025740-create-user-inventories.js
@@ -46,7 +46,7 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
+        type: Sequelize.DATEONLY,
         defaultValue: new Date()
       },
       updatedAt: {
